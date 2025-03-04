32645878f054e486345f6e97a16170f34a9cd4a4
Matthew Miller <matthewjmiller07@gmail.com>
2025-03-04 13:23:30 -0600

    Optimize bundle size with code splitting

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom'],
+          'mui-vendor': ['@mui/material', '@mui/icons-material'],
+          'chart-vendor': ['apexcharts', 'react-apexcharts'],
+          'map-vendor': ['leaflet']
+        }
+      }
+    }
+  },
   plugins: [react()],
   base: '/jewish-zmanim/',
   server: {
